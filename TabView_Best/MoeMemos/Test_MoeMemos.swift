519664a24d4c30364ffbc398f826aae3ded5c611
wwnje <wwnje@qq.com>
2025-11-08 15:36:07 +0800

    save

--- TabView_Best/MoeMemos/Test_MoeMemos.swift
+++ TabView_Best/MoeMemos/Test_MoeMemos.swift
@@ -7,6 +7,8 @@
 
 import SwiftUI
 
+let listItemSymbolList = ["- [ ] ", "- [x] ", "- [X] ", "* ", "- "]
+
 /*
  参考 IceCubes，Moemeno
  */
@@ -14,19 +16,30 @@ struct Test_MoeMemos: View {
     @State private var name = "Taylor Swift"
     @FocusState var isInputActive: Bool
     
+    @State private var text = ""
+    @State private var selection: Range<String.Index>? = nil
+    
+    @FocusState private var focused: Bool
+    
+
     var body: some View {
         NavigationStack {
-            ZStack(alignment: .bottom) {
-                VStack(alignment: .leading) {
-                    TextField("Enter your name", text: $name)
-                        .textFieldStyle(.roundedBorder)
-                        .focused($isInputActive)
-                        .padding(.horizontal)
+            VStack(alignment: .leading) {
+                TextView(text: $text, selection: $selection, shouldChangeText: shouldChangeText(in:replacementText:))
+                    .focused($focused)
+                    .overlay(alignment: .topLeading) {
+                        if text.isEmpty {
+                            Text("input.placeholder")
+                                .foregroundColor(.secondary)
+                                .padding(EdgeInsets(top: 8, leading: 5, bottom: 8, trailing: 5))
+                        }
+                    }
+                    .padding(.horizontal)
 //                    MemoInputResourceView(viewModel: viewModel)
-                }
-                .padding(.bottom, 40)
-                
-                toolbar()
+            }
+            .padding(.bottom, 40)
+            .task {
+                focused = true
             }
 
 //                .toolbar {
@@ -37,11 +50,10 @@ struct Test_MoeMemos: View {
 //                        }
 //                    }
 //                }
-//                .safeAreaInset(edge: .bottom) {
-//                    Button("233") {
-//                        isInputActive = false
-//                    }
-//                }
+                .safeAreaInset(edge: .bottom) {
+                    toolbar()
+
+                }
         }
     }
     
@@ -61,6 +73,7 @@ struct Test_MoeMemos: View {
                 }
                 
                 Button {
+                    focused = false
                 } label: {
                     Image(systemName: "camera")
                 }
@@ -72,6 +85,39 @@ struct Test_MoeMemos: View {
             .padding(.vertical, 12)
             .background(.ultraThinMaterial)
         }
+    }
+    
+    private func shouldChangeText(in range: Range<String.Index>, replacementText text: String) -> Bool {
+        if text != "\n" || range.upperBound != range.lowerBound {
+            return true
+        }
+        
+        let currentText = self.text
+        let contentBefore = currentText[currentText.startIndex..<range.lowerBound]
+        let lastLineBreak = contentBefore.lastIndex(of: "\n")
+        let nextLineBreak = currentText[range.lowerBound...].firstIndex(of: "\n") ?? currentText.endIndex
+        let currentLine: Substring
+        if let lastLineBreak = lastLineBreak {
+            currentLine = currentText[currentText.index(after: lastLineBreak)..<nextLineBreak]
+        } else {
+            currentLine = currentText[currentText.startIndex..<nextLineBreak]
+        }
+        
+        for prefixStr in listItemSymbolList {
+            if (!currentLine.hasPrefix(prefixStr)) {
+                continue
+            }
+            
+            if currentLine.count <= prefixStr.count || currentText.index(currentLine.startIndex, offsetBy: prefixStr.count) >= range.lowerBound {
+                break
+            }
+            
+            self.text = currentText[currentText.startIndex..<range.lowerBound] + "\n" + prefixStr + currentText[range.upperBound..<currentText.endIndex]
+            selection = self.text.index(range.lowerBound, offsetBy: prefixStr.count + 1)..<self.text.index(range.upperBound, offsetBy: prefixStr.count + 1)
+            return false
+        }
+
+        return true
     }
 }
 
